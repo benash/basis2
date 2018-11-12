b6fc652ab91a459d21e58e70fd7abf15baac583d
Benjamin Nash <benash@umich.edu>
2018-11-12 16:07:29 -0500

    Add devtool

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'eval-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
