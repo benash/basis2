79d564e5dbae4ff8af6a012c317b6bf1979e5cb7
Benjamin Nash <benash@umich.edu>
2018-11-13 17:01:37 -0500

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import styled from 'styled-components'
+import App from './App'
 import 'html5-boilerplate/dist/css/normalize.css'
 import 'html5-boilerplate/dist/css/main.css'
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`
-
-ReactDOM.render(<Title>asjf</Title>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
