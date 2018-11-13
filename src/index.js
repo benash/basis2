643a173e0a838d3b5cbfb0e49ac76c4530f0fb02
Benjamin Nash <benash@umich.edu>
2018-11-13 16:14:54 -0500

    Add sytled-components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-ReactDOM.render(<p>asjf</p>, document.getElementById("root"));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+ReactDOM.render(<Title>asjf</Title>, document.getElementById('root'))
