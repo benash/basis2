046c1dd647fd91ea6f8361241c31ec9faa1ab07b
Benjamin Nash <benash@umich.edu>
2018-11-12 21:10:32 -0500

    Add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ],
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin(),
