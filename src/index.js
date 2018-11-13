f677a36b2c723c4f835522f6d40017363ebcb75a
Benjamin Nash <benash@umich.edu>
2018-11-13 16:30:50 -0500

    Add html5-boilerplate CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import styled from 'styled-components'
+import 'html5-boilerplate/dist/css/normalize.css'
+import 'html5-boilerplate/dist/css/main.css'
 
 const Title = styled.h1`
   font-size: 1.5em;
