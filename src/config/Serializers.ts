65b5b35c364d07adc98e90b6d78de00d3dd18f9d
Benjamin Nash <benash@gmail.com>
2018-12-10 13:27:38 -0500

    Serialize simple arrays on single line

--- src/config/Serializers.ts
+++ src/config/Serializers.ts
@@ -10,16 +10,23 @@ const serializeObjectEntries = (o: object, level: number) =>
 const serializeObjectEntry = ([key, val], level: number) =>
   `${indentFor(level)}${key}: ${serializeAny(val, level)},\n`
 
-const serializeArray = (a: Array<any>, level: number) =>
-  '[\n' + serializeArrayElements(a, level + 1) + indentFor(level) + ']'
+const hasComplexElements = (a: any[]) => a.some(el => el instanceof Array || el instanceof Object)
+
+const serializeArray = (a: Array<any>, level: number) => hasComplexElements(a) ?
+  serializeArrayMultipleLines(a, level) : 
+  serializeArraySingleLine(a, level)
+
+const serializeArrayMultipleLines = (a: Array<any>, level: number) =>
+  '[\n' + serializeArrayElements(a, level + 1).join(',\n') + '\n' + indentFor(level) + ']'
+
+const serializeArraySingleLine = (a: Array<any>, level: number) =>
+  '[ ' + serializeArrayElements(a, 0).join(', ') + ' ]'
 
 const serializeArrayElements = (a: Array<any>, level: number) =>
-  a.map(element => serializeArrayElement(element, level)).join('')
+  a.map(element => serializeArrayElement(element, level))
 
 const serializeArrayElement = (val: any, level: number) =>
-  `${indentFor(level)}${serializeAny(val, level)},\n`
-
-const quote = (s: string) => `'${s}'`
+  `${indentFor(level)}${serializeAny(val, level)}`
 
 function serializeAny(a: any, level: number) {
   if (a instanceof RegExp || typeof a === 'string') {
