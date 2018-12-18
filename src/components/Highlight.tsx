9fbcb35d13256e11998e9ca7e365f1f35b64e34c
Benjamin Nash <benash@gmail.com>
2018-12-17 20:43:01 -0500

    Update Highlight.tsx

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -1,21 +1,17 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import styled from 'styled-components'
-
 import ReactHighlight from 'react-highlight'
-
-Highlight.propTypes = {
-  className: PropTypes.string,
-  language: PropTypes.string.isRequired,
-  children: PropTypes.node,
-}
 
 const StyledHighlight = styled(ReactHighlight)`
   font-size: 0.8em;
 `
 
-export default function Highlight(props) {
-  return <StyledHighlight className={props.language} language={props.language}>
-    {React.Children.toArray(props.children).join('')}
-  </StyledHighlight>
+const Highlight: React.SFC<{ language: string }> = (props) => {
+  return (
+    <StyledHighlight className={props.language}>
+      {React.Children.toArray(props.children).join('')}
+    </StyledHighlight>
+  )
 }
+
+export default Highlight
